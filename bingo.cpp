d049ea9dedcc416fa8b766c3d8cfc35b138d00e4
silviafpp <silviapacheco724@gmail.com>
2024-01-26 12:33:52 -0100

    ff

--- bingo.cpp
+++ bingo.cpp
@@ -1,48 +1,35 @@
 #include <iostream>
 #include <iomanip>
+#include <algorithm>
 #include <unistd.h>
 #include <cstdlib>
 using namespace std;
 
 string blue = "\u001b[34m";
+string red = "\u001b[31m";
 string reset = "\u001b[0m";
 string bold = "\u001b[1m";
 
-#define NUMEROS     100
-#define COLUNAS     10
-#define DELAY       1000000 //1 segundo
-#define MODO_AUTO   false
+#define NUMEROS 100
+#define COLUNAS 10
+#define DELAY 1000000
+#define MODO_AUTO false
 
-void clearScreen() {
+void clearScreen()
+{
     cout << "\033[2J\033[1;1H" << flush;
 }
 
-int geraNumero(){
+int geraNumero()
+{
     return rand() % NUMEROS + 1;
 }
 
-void geraChave(int chave[], int quantity){
-    int i = 0, j = 0, aux = 0;
-    bool repetido = false;
-    while(i < quantity){
-        aux = geraNumero();
-        for(j = 0; j < i; j++){
-            if(aux == chave[j]){
-                repetido = true;
-                break;
-            }
-        }
-        if(!repetido){
-            chave[i] = aux;
-            i++;
-        }
-        repetido = false;
-    }
-}
-
-void mainmenu() {
-    cout << blue << bold << "E S C O L H A    A    O P Ç Â O: \n" << reset
-         << blue << "--------------------------\n" 
+void mainmenu()
+{
+    cout << blue << bold << "E S C O L H A    A    O P Ç Â O: \n"
+         << reset
+         << blue << "--------------------------\n"
          << "1 - Sorteio Automático\n"
          << "2 - Sorteio Manual\n"
          << "3 - Gerar Cartões\n"
@@ -52,50 +39,145 @@ void mainmenu() {
          << reset;
 }
 
-int main() {
+void displayNumber(int number, const string &color, const string &label)
+{
+    cout << color << label << ": " << number << reset << endl;
+}
+
+void displayPanel(int pickedAmount, int currentNumber, int sortedNumbers[], int sortedCount)
+{
+    cout << "Picked Amount: " << pickedAmount << "\n";
+    cout << "Panel:\n";
+
+    int totalNumbersToDisplay = min(pickedAmount, NUMEROS);
+
+    for (int i = 1; i <= totalNumbersToDisplay; i++)
+    {
+        bool isCurrent = (i == currentNumber);
+        bool isSorted = false;
+
+        for (int j = 0; j < sortedCount; j++)
+        {
+            if (sortedNumbers[j] == i)
+            {
+                isSorted = true;
+                break;
+            }
+        }
+
+        if (isCurrent)
+        {
+            cout << blue << setw(5) << i;
+        }
+        else if (isSorted)
+        {
+            cout << red << setw(5) << i;
+        }
+        else
+        {
+            cout << blue << setw(5) << i;
+        }
+
+        if (i % COLUNAS == 0)
+        {
+            cout << reset << endl;
+        }
+        else
+        {
+            cout << "\t";
+        }
+    }
+
+    if (totalNumbersToDisplay % COLUNAS != 0)
+    {
+        cout << reset << endl;
+    }
+}
+
+int main()
+{
     int bingotype;
     int quantity;
+    int previousNumber = 0;
+    int currentNumber = 0;
+    const int MAX_PREVIOUS = 5;
+    int previousNumbers[MAX_PREVIOUS];
 
-    do {
+    do
+    {
         clearScreen();
         mainmenu();
         cin >> bingotype;
-        
-        if (bingotype == 1) {
-            clearScreen(); 
+
+        if (bingotype == 1)
+        {
+            clearScreen();
             cout << "You selected 'Automatic Draw'" << endl;
             cout << "Enter the quantity of numbers for the draw (75, 90, 100): ";
             cin >> quantity;
 
-            if ((quantity == 75) || (quantity == 90) || (quantity == 100)) {
+            if ((quantity == 75) || (quantity == 90) || (quantity == 100))
+            {
                 cout << "You entered one of the allowed numbers." << endl;
 
-                int chave[NUMEROS];
-                geraChave(chave, quantity);
+                int sortedNumbers[NUMEROS];
+                int sortedCount = 0;
 
-                for(int i = 0; i < quantity; i++){
-                    cout << chave[i] << "\t";
-                    if((i + 1) % COLUNAS == 0){
-                        cout << endl;
-                    }
+                for (int i = 0; i < MAX_PREVIOUS; i++)
+                {
+                    previousNumbers[i] = 0;
                 }
-                cout << endl;
 
-                // Clear the screen after displaying the numbers
+                for (int i = 0; i < quantity && sortedCount < quantity; i++)
+                {
+                    clearScreen();
+
+                    // Display previous number in red
+                    displayNumber(previousNumber, red, "Previous Number");
+
+                    // Draw a new number
+                    currentNumber = geraNumero();
+
+                    // Display current number in blue
+                    displayNumber(currentNumber, blue, "Current Number");
+
+                    // Display panel with columns
+                    displayPanel(quantity, currentNumber, sortedNumbers, sortedCount);
+
+                    // Update previousNumber with the drawn number
+                    previousNumber = currentNumber;
+
+                    // Update sortedNumbers with the currentNumber
+                    sortedNumbers[sortedCount++] = currentNumber;
+
+                    usleep(DELAY); // Add delay before drawing the next number
+                }
+
+                // Clear the screen after displaying all the numbers
                 cout << "Press Enter to continue...";
                 cin.ignore(); // Wait for Enter key press
                 clearScreen();
-            } else {
+            }
+            else
+            {
                 cout << "Invalid input. The number must be 75, 90, or 100." << endl;
             }
-        } else if (bingotype == 2) {
+        }
+        else if (bingotype == 2)
+        {
             cout << "You selected 'Manual Draw'" << endl;
-        } else if (bingotype == 3) {
+        }
+        else if (bingotype == 3)
+        {
             cout << "You selected 'Generate Cards'" << endl;
-        } else if (bingotype == 4) {
+        }
+        else if (bingotype == 4)
+        {
             cout << "Exiting..." << endl;
-            break; // Exit the loop and end the program
-        } else {
+            break;
+        }
+        else
+        {
             cout << "Invalid Answer, try again or press '4' to quit" << endl;
         }
 
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,110 +1,45 @@
-#include <iostream>
-#include <iomanip>
-#include <unistd.h>
-#include <cstdlib>  // Include for rand function
-using namespace std;
+void displayPanel(int pickedAmount, int currentNumber, int sortedNumbers[], int sortedCount)
+{
+    cout << "Picked Amount: " << pickedAmount << "\n";
+    cout << "Panel:\n";
 
-string blue = "\u001b[34m";
-string reset = "\u001b[0m";
-string bold = "\u001b[1m";
+    int totalNumbersToDisplay = (pickedAmount <= NUMEROS) ? pickedAmount : NUMEROS;
 
-#define NUMEROS     100
-#define COLUNAS     10
-#define DELAY       1000000 //1 segundo
-#define MODO_AUTO   false
+    for (int i = 1; i <= totalNumbersToDisplay; i++)
+    {
+        bool isCurrent = (i == currentNumber);
+        bool isSorted = false;
 
-void clearScreen() {
-    cout << "\033[2J\033[1;1H" << flush;
-}
-
-int geraNumero(){
-    return rand() % NUMEROS + 1;
-}
-
-void geraChave(int chave[]){
-    int i = 0, j = 0, aux = 0;
-    bool repetido = false;
-    while(i < NUMEROS){
-        aux = geraNumero();
-        for(j = 0; j < i; j++){
-            if(aux == chave[j]){
-                repetido = true;
-                break;
-            }
+        if (i <= sortedCount)
+        {
+            isSorted = (find(sortedNumbers, sortedNumbers + sortedCount, i) != sortedNumbers + sortedCount);
         }
-        if(!repetido){
-            chave[i] = aux;
-            i++;
+
+        if (isCurrent)
+        {
+            cout << blue << setw(5) << i;
         }
-        repetido = false;
+        else if (isSorted)
+        {
+            cout << red << setw(5) << i;
+        }
+        else
+        {
+            cout << blue << setw(5) << i;
+        }
+
+        if (i % COLUNAS == 0)
+        {
+            cout << reset << endl;
+        }
+        else
+        {
+            cout << "\t";
+        }
     }
-}
 
-void mainmenu() {
-    cout << blue << bold << "E S C O L H A    A    O P Ç Â O: \n" << reset
-         << blue << "--------------------------\n" 
-         << "1 - Sorteio Automático\n"
-         << "2 - Sorteio Manual\n"
-         << "3 - Gerar Cartões\n"
-         << "4 - Sair\n"
-         << "--------------------------\n"
-         << bold << "O P Ç Ã O: "
-         << reset;
-}
-
-int main() {
-    int bingotype;
-    int quantity;
-
-    do {
-        clearScreen();
-        mainmenu();
-        cin >> bingotype;
-        
-        if (bingotype == 1) {
-            clearScreen(); 
-            cout << "You selected 'Automatic Draw'" << endl;
-            cout << "Enter the quantity of numbers for the draw (75, 90, 100): ";
-            cin >> quantity;
-
-            if ((quantity == 75) || (quantity == 90) || (quantity == 100)) {
-                cout << "You entered one of the allowed numbers." << endl;
-            } else {
-                cout << "Invalid input. The number must be 75, 90, or 100." << endl;
-            }
-        } else if (bingotype == 2) {
-            cout << "You selected 'Manual Draw'" << endl;
-        } else if (bingotype == 3) {
-            cout << "You selected 'Generate Cards'" << endl;
-        } else if (bingotype == 4) {
-            cout << "Exiting..." << endl;
-            return 0;
-        } else {
-            cout << "Invalid Answer, try again or press '4' to quit" << endl;
-            break;
-        }
-
-    } while (true);
-
-
-    do {
-        cout << "\033[2J\033[1;1H"; // Clear the screen
-        int chave[NUMEROS];
-        geraChave(chave);
-
-        // Display numbers sequentially with a 1-second pause
-        for(int i = 0; i < NUMEROS; i++){
-            cout << chave[i];
-            usleep(DELAY);
-            cout << "\t" << flush; 
-            if((i + 1) % COLUNAS == 0){
-                cout << endl;
-            }
-        }
-        cout << endl;
-        cout << "Do you want to generate another bingo sequence? (1 for yes, 0 for no): ";
-        cin >> bingotype;
-    } while (bingotype == 1);
-
-    return 0;
+    if (totalNumbersToDisplay % COLUNAS != 0)
+    {
+        cout << reset << endl;
+    }
 }
